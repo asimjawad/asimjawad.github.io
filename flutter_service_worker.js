93e19729e6d30a0e4248aa7c35bc7ac2cb31e630
Muhammad Asim Jawad <asimjawad2723@gmail.com>
2023-08-11 01:54:30 +0500

    minor changes 01

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "009c9e65172e010890f7f65fde438006",
 "index.html": "d1a05f3681418d672d60f370d6956311",
 "/": "d1a05f3681418d672d60f370d6956311",
-"main.dart.js": "9fba34ab7b167cd5806a5f52433bb39f",
+"main.dart.js": "9039dcf16ed920fdae4e852341e0058c",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
